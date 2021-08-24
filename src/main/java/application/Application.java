413e4fd9bddb9305825d1f8ddacbbc7af72caf3b
Ricardo Santos <ricardosds@gmail.com>
2021-08-24 18:55:44 +0000

    criação dos pacotes

---
+++ src/main/java/application/Application.java
@@ -0,0 +1,10 @@
+package application;
+public class Application{
+    public static void main(String args[]){
+
+        //git add .
+        //git commit -m "criação dos pacotes"
+        //git push
+
+    }
+}
